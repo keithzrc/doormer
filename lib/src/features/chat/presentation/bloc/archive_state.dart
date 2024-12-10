5af9ffe68647b70b73e249c03ab97fb1bc86434e
Wendy-sunny1 <140019411+Wendy-sunny1@users.noreply.github.com>
2024-12-10 14:18:23 +1300

    archive_state.dart modified

--- lib/src/features/chat/presentation/bloc/archive_state.dart
+++ lib/src/features/chat/presentation/bloc/archive_state.dart
@@ -1,22 +1,34 @@
-// archieve_state.dart
+import 'package:equatable/equatable.dart';
 import '../../domain/entities/chat_entity.dart';
 
-abstract class ChatState {}
+abstract class ChatState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class ChatLoadingState extends ChatState {}
 
 class ChatLoadedState extends ChatState {
   final List<Chat> chats;
+
   ChatLoadedState(this.chats);
+
+  @override
+  List<Object?> get props => [chats];
 }
 
 class ArchivedChatLoadedState extends ChatState {
   final List<Chat> archivedChats;
+
   ArchivedChatLoadedState(this.archivedChats);
+
+  @override
+  List<Object?> get props => [archivedChats];
 }
 
 class ChatErrorState extends ChatState {
   final String error;
+
   ChatErrorState(this.error);
 
   @override
