8ff4b4eebe0f46cb545a27f694aaa372d193cb03
Keith Chuang <101155002+keithzrc@users.noreply.github.com>
2024-12-19 10:49:01 +1300

    moved toggle archive logic to usecase

--- lib/src/features/chat/domain/usecases/archive_chat_usecases.dart
+++ lib/src/features/chat/domain/usecases/archive_chat_usecases.dart
@@ -2,21 +2,26 @@ import '../repositories/chat_repository.dart';
 import '../entities/contact_entity.dart';
 
 /// Use case for toggle a chat.
-/// This class interacts with the ChatRepository to toggle a chat unarchived to archived
-/// identified by its chatId.
+/// This class toggles the isArchived status of chat and updates with the ChatRepository
 class ToggleChatArchivedStatus {
   final ChatRepository repository;
 
   ToggleChatArchivedStatus(this.repository);
 
-  Future<void> call(String chatId) async {
-    await repository.toggleChat(chatId);
+  Future<Contact> call(Contact contact) async {
+    final updatedContact = contact.copyWith(isArchived: !contact.isArchived);
+
+    // TODO: maybe just create a updateArchiveStatus(), where it Updates the `isArchived` status of a chat by its ID.
+    await repository.updateChat(updatedContact);
+
+    return updatedContact;
   }
 }
 
 /// Use case for deleting a chat.
 /// This class interacts with the ChatRepository to delete a chat
 /// identified by its chatId.
+// TODO: Move to centralized/ active chat usecase file
 class DeleteChat {
   final ChatRepository repository;
 
@@ -35,19 +40,19 @@ class GetArchivedChatList {
   GetArchivedChatList(this.repository);
 
   Future<List<Contact>> call() async {
-    return await repository.getArchivedList();
+    return await repository.getArchivedChatList();
   }
 }
 
-/// Use case for retrieving the list of unactived chats.
-/// This class interacts with the ChatRepository to fetch all unactived chats
-/// that are not archived.
+/// Use case for retrieving the list of Active chats.
+/// This class interacts with the ChatRepository to fetch all Active chats
+// TODO: Move to centralized/ active chat usecase file
 class GetActiveChatList {
   final ChatRepository repository;
 
   GetActiveChatList(this.repository);
 
   Future<List<Contact>> call() async {
-    return await repository.getUnarchivedchatList();
+    return await repository.getActiveChatList();
   }
 }
