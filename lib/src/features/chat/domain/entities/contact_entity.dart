e86aa7a1badd0d670281d7923ea13e423098740d
Keith Chuang <101155002+keithzrc@users.noreply.github.com>
2024-12-25 14:25:18 +1300

    Used freezed for Contact Entity.

--- lib/src/features/chat/domain/entities/contact_entity.dart
+++ lib/src/features/chat/domain/entities/contact_entity.dart
@@ -1,50 +1,33 @@
-/// Represents a chat entity in the system.
-/// This class holds all the necessary information about a chat, including:
-/// - [uuid]: Unique identifier for the chat.
-/// - [userName]: Name of the user associated with the chat.
-/// - [avatarUrl]: URL of the user's avatar image.
-/// - [lastMessage]: The last message sent or received in the chat.
-/// - [createdTime]: The timestamp of when the chat was created (optional).
-/// - [isArchived]: Indicates whether the chat is archived.
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 
-// TODO: User inside of Contact?
-class Contact {
-  final String id; // Uuid as String
-  final String userName;
-  final String avatarUrl;
-  final String lastMessage;
-  final DateTime? lastMessageCreatedTime;
-  final bool isArchived;
-  final bool isRead;
+part 'contact_entity.freezed.dart';
 
-  Contact({
-    required this.id,
-    required this.userName,
-    required this.avatarUrl,
-    required this.lastMessage,
-    required this.lastMessageCreatedTime,
-    required this.isArchived,
-    required this.isRead,
-  });
+/// Represents a contact entity in the domain layer.
+///
+/// This immutable data class encapsulates core business logic related to a contact.
+@freezed
+class Contact with _$Contact {
+  const factory Contact({
+    /// Unique identifier for the contact, represented as a UUID.
+    required UuidValue id,
 
-  Contact copyWith({
-    String? id,
-    String? userName,
-    String? avatarUrl,
-    String? lastMessage,
+    /// Name of the user associated with the contact.
+    required String userName,
+
+    /// URL of the user's avatar image.
+    required String avatarUrl,
+
+    /// The last message sent or received in the chat associated with the contact.
+    required String lastMessage,
+
+    /// The timestamp of when the last message was created.
     DateTime? lastMessageCreatedTime,
-    bool? isArchived,
-    bool? isRead,
-  }) {
-    return Contact(
-      id: id ?? this.id,
-      userName: userName ?? this.userName,
-      avatarUrl: avatarUrl ?? this.avatarUrl,
-      lastMessage: lastMessage ?? this.lastMessage,
-      lastMessageCreatedTime:
-          lastMessageCreatedTime ?? this.lastMessageCreatedTime,
-      isArchived: isArchived ?? this.isArchived,
-      isRead: isRead ?? this.isRead,
-    );
-  }
+
+    /// Indicates whether the contact's chat is archived.
+    required bool isArchived,
+
+    /// Indicates whether the contact's chat has been read.
+    required bool isRead,
+  }) = _Contact;
 }
