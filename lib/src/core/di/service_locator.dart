4d4e2c648bcdd979b0ddffed40f1546f0c5d23b9
Keith Chuang <101155002+keithzrc@users.noreply.github.com>
2025-01-17 21:17:09 +1300

    service_locator update

--- lib/src/core/di/service_locator.dart
+++ lib/src/core/di/service_locator.dart
@@ -1,8 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:doormer/src/core/network/dio_client.dart';
+import 'package:doormer/src/core/network/request_manager.dart';
 import 'package:doormer/src/core/services/sessions/session_service.dart';
-import 'package:doormer/src/core/utils/token_storage.dart';
+import 'package:doormer/src/core/services/sessions/session_service_impl.dart';
+import 'package:doormer/src/core/utils/token_storage/token_storage.dart';
+import 'package:doormer/src/core/utils/token_storage/token_storage_mobile.dart';
+import 'package:doormer/src/core/utils/token_storage/token_storage_web.dart';
 import 'package:doormer/src/features/chat/di/chat_module.dart';
+import 'package:doormer/src/shared/sessions/bloc/global_session_bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:doormer/src/features/auth/di/auth_module.dart';
@@ -10,23 +16,48 @@ import 'package:doormer/src/features/auth/di/auth_module.dart';
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
-  // Register Dio Client with base URL and interceptors
-  serviceLocator.registerLazySingleton<Dio>(() => DioClient.createDio());
-
-  // Register FlutterSecureStorage
-  serviceLocator.registerLazySingleton<FlutterSecureStorage>(
-    () => const FlutterSecureStorage(),
-  );
-
-  // Register TokenStorage
-  serviceLocator.registerLazySingleton(() =>
-      TokenStorage(secureStorage: serviceLocator<FlutterSecureStorage>()));
+  // Register TokenStorage based on platform
+  if (kIsWeb) {
+    // Use Web Implementation
+    serviceLocator.registerSingleton<TokenStorage>(TokenStorageWeb());
+  } else {
+    // Use Mobile Implementation
+    serviceLocator
+        .registerSingleton<FlutterSecureStorage>(const FlutterSecureStorage());
+    serviceLocator.registerSingleton<TokenStorage>(
+      TokenStorageMobile(serviceLocator<FlutterSecureStorage>()),
+    );
+  }
 
   // Register SessionService
-  serviceLocator.registerLazySingleton<SessionService>(
-    () => SessionServiceImpl(
+  serviceLocator.registerSingleton<SessionService>(
+    SessionServiceImpl(
       tokenStorage: serviceLocator<TokenStorage>(),
-      // dio: serviceLocator<Dio>(),
+      dio: serviceLocator<Dio>(),
+    ),
+  );
+
+  // Register GlobalSessionBloc
+  serviceLocator.registerSingleton<GlobalSessionBloc>(
+    GlobalSessionBloc(
+      sessionService: serviceLocator<SessionService>(),
+    ),
+  );
+
+  // Register Dio Client with interceptors
+  serviceLocator.registerSingleton<Dio>(
+    DioClient.createDio(
+      sessionService: serviceLocator<SessionService>(),
+      sessionBloc: serviceLocator<GlobalSessionBloc>(),
+    ),
+  );
+
+  // Register RequestManager with Dio, SessionService, and GlobalSessionBloc
+  serviceLocator.registerSingleton<RequestManager>(
+    RequestManager(
+      dio: serviceLocator<Dio>(),
+      sessionService: serviceLocator<SessionService>(),
+      sessionBloc: serviceLocator<GlobalSessionBloc>(),
     ),
   );
 
