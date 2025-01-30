ddcd53bdc7458f4890e1ab4d3422602a7775cb65
Keith Chuang <101155002+keithzrc@users.noreply.github.com>
2025-01-31 11:39:11 +1300

    added basic user type

--- lib/src/shared/user/user_type.dart
+++ lib/src/shared/user/user_type.dart
@@ -1,11 +1,13 @@
 import 'package:doormer/src/core/utils/app_logger.dart';
 
-enum UserType { candidate, employer }
+enum UserType { basic, candidate, employer }
 
 extension UserTypeExtension on UserType {
   // Convert `UserType` to API string
   String toApiString() {
     switch (this) {
+      case UserType.basic:
+        return 'basic';
       case UserType.candidate:
         return 'candidate';
       case UserType.employer:
@@ -16,6 +18,8 @@ extension UserTypeExtension on UserType {
   // Convert API string to `UserType`
   static UserType fromApiString(String userType) {
     switch (userType) {
+      case 'basic':
+        return UserType.basic;
       case 'candidate':
         return UserType.candidate;
       case 'employer':
