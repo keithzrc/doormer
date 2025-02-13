9e16a79cbeca9b5a711c9a33d31e3d4d4b799dbe
Keith Chuang <101155002+keithzrc@users.noreply.github.com>
2025-02-14 11:18:34 +1300

    config update

--- lib/src/core/config/app_config.dart
+++ lib/src/core/config/app_config.dart
@@ -1,10 +1,12 @@
 class AppConfig {
-  static const String apiBaseUrl =
-      'https://728186ec-63ae-44b3-be57-bfa793127d5f.mock.pstmn.io';
+  // TODO: put base url
+  static const String apiBaseUrl = '';
   static const int connectTimeout = 15000; // in milliseconds
   static const int receiveTimeout = 15000;
 
   // Add environment-specific keys or configurations
   static const String googleApiKey = 'GOOGLE_API_KEY_HERE';
   static const String appVersion = '1.0.0';
+  // TODO: put googleClientId
+  static const String googleClientId = '';
 }
